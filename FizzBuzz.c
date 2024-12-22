2e8245f7a6a3787c48555fd24e1414a15de7bead
boris-valero <157314735+boris-valero@users.noreply.github.com>
2024-12-22 12:45:10 +0000

    Update FizzBuzz.c

--- FizzBuzz.c
+++ FizzBuzz.c
@@ -2,22 +2,22 @@
 #include <stdio.h>
 
 int main (int argc, char *argv[]) {
-int chiffre = 0;
-while (chiffre <= 100) {
+	int chiffre = 0;
+	while (chiffre <= 100) {
 	chiffre++;
-if (chiffre % 15 == 0){
-printf("FizzBuzz\n");
-}
-else if (chiffre % 5 == 0){
-printf("Buzz\n");
-}
-else if (chiffre % 3 == 0){
-printf("Fizz\n");
-}
-else{
-	printf("%d\n", chiffre);
-}
-}
+		if (chiffre % 15 == 0){
+		printf("FizzBuzz\n");
+		}
+		else if (chiffre % 5 == 0){
+		printf("Buzz\n");
+		}
+		else if (chiffre % 3 == 0){
+		printf("Fizz\n");
+		}
+		else{
+		printf("%d\n", chiffre);
+		}
+	}
 return 0;
 }
 
